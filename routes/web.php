869f42d8b3111b5601f3ed358ef70dbbbe77804a
PurplishCode <darellpetualang@gmail.com>
2024-03-29 11:35:23 -0700

    commited first

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['guest'])->group(function(){
+    Route::get('/register', function(){
+        return view('auth.register');
+    });
+});
